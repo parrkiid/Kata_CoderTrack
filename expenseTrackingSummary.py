5af1eb20636d0d6da8f0d7ffb58fc9d289fc8ff0
Pichaya Penjan <pichaya@Par-MacBook.local>
2018-09-25 22:58:05 +0700

    Add Code for Solve Problem NO#5-expense_avg

---
+++ expenseTrackingSummary.py
@@ -0,0 +1,15 @@
+with open('./ExpenseTrackingData.txt') as f:
+    INPUT = f.read().splitlines()
+
+SUM_EXPENSE = 0
+DAYS = len(INPUT)
+for row in INPUT:
+    PER_DAY_EXPENSE = row.split()
+    if len(PER_DAY_EXPENSE) > 1:
+        for i in range(len(PER_DAY_EXPENSE)-1):
+            #print(PER_DAY_EXPENSE[i+1][1:])
+            SUM_EXPENSE += float(PER_DAY_EXPENSE[i+1][1:])
+#print(SUM_EXPENSE)
+#print(DAYS)
+print("{:.2f}".format(SUM_EXPENSE/DAYS))
+    
